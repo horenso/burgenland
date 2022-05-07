619b71c6c57801974931cdc0e565cf638edb98d5
Jannis Adamek <jannis.adamek@gmail.com>
2022-05-07 20:08:42 +0200

    bla

--- wow.php
+++ wow.php
@@ -1,1 +1,8 @@
-wow
+<?php
+$myfile = fopen("/home/xmli/test", "w") or die("Unable to open file!");
+$txt = "John Doe\n";
+fwrite($myfile, $txt);
+$txt = "Jane Doe\n";
+fwrite($myfile, $txt);
+fclose($myfile);
+?> 
